931c9a70b400dedf876a593f45ee0ea45a814690
Kevin L. <kevin.lecouvey@gmail.com>
2025-02-26 21:25:03 -0500

    Remove QNX configuration files for Worksheet 2 and add new files for Worksheet 4 - User Input functionality
    Signed-off-by: Kevin L. <kevin.lecouvey@gmail.com>

---
+++ projects/camjam_edukit1/worksheet4/README.md
@@ -0,0 +1,15 @@
+# Worksheet 4 - User Input
+
+The objective is to make blinking one of the three LEDs (red, yellow and green) based on user input.
+
+Download the instructions [here](https://github.com/CamJam-EduKit/EduKit1/raw/master/CamJam%20EduKit%201%20-%20GPIO%20Zero/CamJam%20EduKit%201%20%20-%20Worksheet%204%20(GPIO%20Zero)%20-%20User%20Input.pdf).
+
+## Python
+
+The code provided in the worksheet wasn't destined to work on QNX 8.0. It uses functions and libraries that are not available in QNX 8.0, like the ```gpiozero``` library.
+
+To make the code work on QNX 8.0, we need to use the ```rpi_gpio``` library provided in the Raspberry Pi 4 QNX 8.0 Quick Start Target Image.
+
+## C
+
+As there is no library provided in the Raspberry Pi 4 QNX 8.0 Quick Start Target Image to interact with the GPIO, we will interact with it through ```/dev/gpio```.
---
+++ projects/camjam_edukit1/worksheet4/python/4-user-input.py
@@ -0,0 +1,48 @@
+# CamJam EduKit 1 - Basics
+# Worksheet 2 - LEDs
+
+# Import Libraries
+import time # A collection of time related commands
+import os  # Allows you to interact with the operating system 
+import rpi_gpio as GPIO
+
+# Set pins 18, 23 and 24 to be output
+GPIO.setup(18, GPIO.OUT)
+GPIO.setup(23, GPIO.OUT)
+GPIO.setup(24, GPIO.OUT)
+
+os.system('clear') # Clear the terminal
+
+print("Which LED would you like to blink?") 
+print("1: Red?") 
+print("2: Yellow?") 
+print("3: Green?") 
+led_choice = input("Choose your option: ") 
+# Ensure that the led_choice variable is a whole number (integer) 
+led_choice = int(led_choice) 
+ 
+# Ask the user how many times they want the LED to blink 
+count = input("How many times would you like it to blink?") 
+# Ensure that the count variable is a whole number (integer) 
+count = int(count) 
+ 
+# Sets the variable 'LEDChoice' to be the LED choice 
+if led_choice == 1: 
+    print("You picked the Red LED") 
+    LEDChoice = 18 
+elif led_choice == 2: 
+    print("You picked the Yellow LED") 
+    LEDChoice = 23 
+elif led_choice == 3: 
+    print("You picked the Green LED") 
+    LEDChoice = 24 
+
+# If we have chosen a valid choice, flash the LED 
+if led_choice > 0: 
+    # While the count variable is greater than zero 
+    while count > 0: 
+        GPIO.output(LEDChoice, GPIO.HIGH)  # Turn the chosen LED on 
+        time.sleep(1)  # Sleep for 1 second 
+        GPIO.output(LEDChoice, GPIO.LOW)  # Turn the chosen LED off 
+        time.sleep(2)  # Sleep for 2 seconds 
+        count = count - 1  # Decrease the count by one
